4a139119dc9109076409c69fc95d5e96ee44bc1a
lars-brg <lara.lbraga@alu.ufc.br>
2025-02-28 14:22:16 -0300

    :sparkles: feat: added highlights

--- momentus/app/page.tsx
+++ momentus/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "./components/navbar";
 import Hero from "./components/hero";
+import Highlight from "./components/highlight";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero/>
+      <Highlight/>
     </div>
   );
 }
